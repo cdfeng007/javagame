a70bf3c5610f2dc2dd3568b408119e01f150d3e9
cdfeng <cdf007@126.com>
2019-09-25 20:37:18 +0800

    Signed-off-by: cdfeng <cdf007@126.com>

--- src/Arr.java
+++ src/Arr.java
@@ -1,67 +1,93 @@
-import java.awt.Color;
+import java.awt.Dimension;
+
+
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+
 import com.rupeng.game.GameCore;
 
-public class Arr implements Runnable {
+public class Arr implements Runnable {	
+	
 
 	public void run() {
-		String[] names = {"一","二","三","四","五"};
-		for(int num=0;num<names.length;num++){
-			GameCore.createText(num, names[num]);
-			GameCore.setTextPosition(num, 40, num*50);
-		}
 		
-		int i=0;
-		boolean fx = true;
+		int girlNum = 0;
+		GameCore.createSprite(girlNum, "girl");
+		GameCore.setSpritePosition(girlNum, 0, 0);
+		GameCore.playSpriteAnimate(girlNum, "walk",true);
+		Dimension gameSize = GameCore.getGameSize();
+		int gameWidth = gameSize.width;
+		int gameHeight = gameSize.height;
+		Dimension girlSize = GameCore.getSpriteSize(girlNum);
+		int girlWidth = girlSize.width;
+		int girlHeight = girlSize.height;
 		
-		while(i < names.length){
-			int j=0;
-			while (j < names.length){
-				if(j != i){
-					GameCore.setTextColor(j, Color.BLACK);					
-				}
-				j++;
-			}
-			GameCore.setTextColor(i, Color.red);
-			if(i == names.length-1 && fx ){
-				fx = false;
-			}else if(i==0 && !fx){
-				fx = true;
-			}
-			
-			if(fx){
-				i++;
-			}
-			if(!fx){
-				i--;
-			}
-			
-			GameCore.pause(300);
-			
-			
-		}
-
+		int tuoluoNum = 1;
+		GameCore.createSprite(tuoluoNum, "tuoluo");
+		GameCore.setSpritePosition(tuoluoNum, 200,200);
+		GameCore.playSpriteAnimate(tuoluoNum, "rotate", true);
+		Point tuoluoPos = GameCore.getSpritePosition(tuoluoNum);
 		
-		/*for(int i=0;i<names.length;i++){
-			for(int j=0;j<names.length;j++){
-				if(j != i){
-					GameCore.setTextColor(j, Color.BLACK);
-				}
-			}
-			GameCore.setTextColor(i, Color.red);	
-			GameCore.pause(400);
-			if(i == names.length-1){
-				i = -1;
-			}
-		}*/
+		int bombNum = 2;
+		GameCore.createSprite(bombNum, "bomb");
+		GameCore.setSpritePosition(bombNum, 200, 200);
+		GameCore.playSpriteAnimate(bombNum, "fire",true);
+		GameCore.hideSprite(bombNum);
 		
+			
 		for(;;){
-			GameCore.pause(100);
+			int kc = GameCore.getPressedKeyCode();
+			Point pos = GameCore.getSpritePosition(girlNum);
+			if(kc == KeyEvent.VK_LEFT){
+				if(pos.x>0){
+					GameCore.setSpriteFlipX(girlNum, true);
+					GameCore.setSpritePosition(girlNum, pos.x-1, pos.y);
+				}
+				
+			}
+			if(kc == KeyEvent.VK_RIGHT){
+				if(pos.x < gameWidth-girlWidth){
+					GameCore.setSpriteFlipX(girlNum, false);
+					GameCore.setSpritePosition(girlNum, pos.x+1, pos.y);
+				}
+				
+			}
+			
+			if(kc == KeyEvent.VK_UP){
+				if(pos.y>0)				
+				GameCore.setSpritePosition(girlNum, pos.x, pos.y-1);
+			}
+			if(kc == KeyEvent.VK_DOWN){
+				if(pos.y < gameHeight-girlHeight-30)
+				GameCore.setSpritePosition(girlNum, pos.x, pos.y+1);
+			}
+			
+			double juli = Math.sqrt(Math.abs((tuoluoPos.x - pos.x)*(tuoluoPos.x-pos.x)+(tuoluoPos.y - pos.y)*(tuoluoPos.y - pos.y)));
+			
+			int jl = (int)juli;
+			
+			if(jl<50){
+				GameCore.hideSprite(tuoluoNum);
+				GameCore.showSprite(bombNum);
+				GameCore.alert("我要死了");
+				GameCore.pause(1000);
+				GameCore.exit();
+				
+			}
+			
+			
+			GameCore.pause(20);
 		}
+        
 	}
+	
+	
 
 	public static void main(String[] args) {
 	
 		GameCore.start(new Arr());
+			
+	
 	}
 
 }
